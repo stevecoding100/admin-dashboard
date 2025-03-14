55c46f75f2ca8308532f3d2b65ec8166184afe5c
Steve De La Rosa <stevedelarosa1036@gmail.com>
2025-03-14 16:46:54 -0400

    updated line chart - adding color

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -43,7 +43,7 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
                     },
                 },
             }}
-            colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
+            colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }}
             margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
             xScale={{ type: "point" }}
             yScale={{
